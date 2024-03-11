0ad92111051589f4686413f039c6ff5f76c3f8ea
starbvuks <starbvuks@gmail.com>
2024-03-11 10:09:47 +0530

    cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,37 +1,44 @@
 module.exports = [
-  'strapi::errors',
+  "strapi::errors",
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
             "'self'",
-            'data:',
-            'blob:',
-            'dl.airtable.com',
-            'soad-strapi-s3-bucket.s3.us-east-1.amazonaws.com',
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "soad-strapi-s3-bucket.s3.us-east-1.amazonaws.com",
           ],
-          'media-src': [
+          "media-src": [
             "'self'",
-            'data:',
-            'blob:',
-            'dl.airtable.com',
-            'soad-strapi-s3-bucket.s3.us-east-1.amazonaws.com',
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "soad-strapi-s3-bucket.s3.us-east-1.amazonaws.com",
           ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: ["http://ec2-54-84-35-62.compute-1.amazonaws.com:1337"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
